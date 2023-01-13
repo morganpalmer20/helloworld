636fb116d7046134d7bedbc6bf3d1b1e135d8883
morganpalmer20 <122543644+morganpalmer20@users.noreply.github.com>
2023-01-13 09:54:04 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,3 +5,5 @@ input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Heart)
 })
 basic.showIcon(IconNames.Skull)
+music.playMelody("C5 F A E B G A C5 ", 292)
+music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.ForeverInBackground)
